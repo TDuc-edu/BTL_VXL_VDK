e612b7c6f2d05392394f3d38f0393bf643a424df
TD <duc81.edu@gmail.com>
2025-12-12 22:58:04 +0700

    update light traffic

---
+++ traffic_light/Core/Inc/light_traffic.h
@@ -0,0 +1,20 @@
+/*
+ * light_traffic.h
+ *
+ *  Created on: Dec 11, 2025
+ *      Author: TD
+ */
+
+#ifndef INC_LIGHT_TRAFFIC_H_
+#define INC_LIGHT_TRAFFIC_H_
+void led_red_and_green();
+void led_red_and_yellow();
+void led_green_and_red();
+void led_yellow_and_red();
+void all_led_red_on();
+void all_led_red_off();
+void all_led_yellow_on();
+void all_led_yellow_off();
+void all_led_green_on();
+void all_led_green_off();
+#endif /* INC_LIGHT_TRAFFIC_H_ */
